517cd10d4f76d15c84db9add54bda6135f8dd60a
MarcinBalejko <mbbm9@o2.pl>
2020-03-01 00:59:33 +0100

    :white_check_mark: Balance and Header components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
+    </div>
+  );
 }
 
 export default App;
